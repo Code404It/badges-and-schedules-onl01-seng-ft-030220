d4a56854cd40c076f1e5253841120de6509eb8e2
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 15:44:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,9 @@ def batch_badge_creator(array)
   array.each{|name| new_array << "Hello, my name is #{name}."}
   new_array
 end
+def assign_rooms(speakers)
+    array = []
+     room = 1
+     speakers.each do |name|
+       array.push("Hello, #{name}! You'll be assigned to room #{room}!")
+     room += 1
