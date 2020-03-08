5208501526899d95ed880c21f918a053812a63c9
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-08 15:45:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,3 +15,11 @@ def assign_rooms(speakers)
 end
    array
 end
+def printer(attendees)
+   batch_badge_creator(attendees).each do |badge|
+     puts badge
+ end
+   assign_rooms(attendees).each do |array|
+     puts array
+   end
+ end
